2f0e690389c34058b19be30a553cfc3489914474
Federico <federico.serratorevr@gmail.com>
2024-03-15 23:35:45 +0100

    add: tailwind to TitleScreen

--- src/pages/Home/components/1_TitleScreen/TitleScreen.tsx
+++ src/pages/Home/components/1_TitleScreen/TitleScreen.tsx
@@ -1,31 +1,48 @@
 export default function TitleScreen() {
+
+    const NavLink = ({href, children}: {href: string, children: string}) => {
+        return (
+            <a className="text-blue-800 hover:underline"
+             href={href}>{children}</a>
+        )
+    }
+    const DescriptionSpan = ({children, animation}: {children: string, animation: string}) => {
+        return (
+            <span  className={`opacity-0 text-start ${animation}`}>{children}</span>
+        )
+    }
+
     return (
-        <div className="h-svh text-center">
-            <nav>
-                <a href="https://github.com/FoFFolo">GitHub</a>
-                <a href="#Contacts">Contacts</a>
+        <div className="h-svh bg-titlescreen-bg bg-[length:30em] bg-repeat
+                        flex flex-col justify-between">
+            <nav className="py-4 px-8 flex flex-row justify-end gap-6
+                            font-bold [font-size:_clamp(1em,4vw,1.3em)]">
+                <NavLink href="https://github.com/FoFFolo">GitHub</NavLink>
+                <NavLink href="#Contacts">Contacts</NavLink>
             </nav>
 
-            <div className="title">
-                <div className="fullname">
-                    <h1 id="surname">Serratore</h1>
-                    <h1 id="name">Federico</h1>
+            <div className="flex flex-col justify-center items-center text-center">
+                <div className="[font-size:_clamp(2em,7vw,3.5em)] font-righteous 
+                                tracking-[.20rem] not-italic text-blue-950">
+                    <h1 className="border-r-[.1em] overflow-hidden whitespace-nowrap my-0 mx-auto animate-surname_anim">Serratore</h1>
+                    <h1 className="w-0 overflow-hidden whitespace-nowrap my-0 mx-auto animate-name_anim">Federico</h1>
                 </div>
-                <div className="description">
+                <div className="font-kodemono text-lg text-green-700">
                     <div className="first__sentence">
                         <span>{">"} </span>
-                        <span className="first">Developer </span>
-                        <span className="second">for</span>
+                        <DescriptionSpan animation="animate-first_anim">Developer </DescriptionSpan>
+                        <DescriptionSpan animation="animate-second_anim">for</DescriptionSpan>
                     </div>
-                    <span className="third">everything</span>
+                    <DescriptionSpan animation="animate-third_anim">everything</DescriptionSpan>
                 </div>
             </div>
 
-            <div className="sections-list__container">
-                <div className="links">
-                    <a href="#Aboutme">About Me</a>
-                    <a href="#Abilities">Abilities</a>
-                    <a href="#Projects">Projects</a>
+            <div className="[font-size:_clamp(1em,4vw,1.3em)] font-bold
+                            flex justify-center py-4">
+                <div className="w-full flex justify-evenly">
+                    <NavLink href="#Aboutme">About Me</NavLink>
+                    <NavLink href="#Abilities">Abilities</NavLink>
+                    <NavLink href="#Projects">Projects</NavLink>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const surnameAnimDuration = .8;
+const nameAnimDelay = surnameAnimDuration - .1;
+const nameAnimDuration = .8;
+const descriptionDelay = surnameAnimDuration + nameAnimDuration
+
+const firstAnimDuration = .7;
+const secondAnimDelay = descriptionDelay + firstAnimDuration + .2;
+const thirdAnimDelay = secondAnimDelay + .2;
+const thirdAnimDuration = .3;
+
 export default {
   content: [
     "./index.html",
@@ -8,8 +18,40 @@ export default {
     extend: {
       colors: {
         primary: '#202225',
+      },
+      backgroundImage: {
+        'titlescreen-bg': "url('../assets/title_bg.png')",
       }
     },
+    fontFamily: {
+      'righteous': ['Righteous', 'sans-serif'],
+      'kodemono': ['Kode Mono', 'monospace'],
+    },
+    keyframes: {
+      typing: {
+        'from': { width: '0' },
+        'to': { width: '100%' },
+      },
+      blink_caret: {
+        '0%, 99%': { borderRight: '.1em solid black' },
+        '100%': { borderRight: 'transparent' },
+      },
+      textAppears: {
+        'from': { opacity: '0' },
+        'to': { opacity: '1' }
+      }
+    },
+    animation: {
+      typing: `typing ${surnameAnimDuration}s steps(9, end) forwards`,
+      blink_caret: `blink_caret ${nameAnimDuration}s step-end forwards`,
+      surname_anim: `typing ${surnameAnimDuration}s steps(9, end) forwards, 
+                    blink_caret ${nameAnimDuration}s step-end forwards`,
+      name_anim: `typing ${nameAnimDuration}s steps(8, end) ${nameAnimDuration}s forwards, 
+                  blink_caret ${nameAnimDuration}s step-end ${nameAnimDuration}s forwards`,
+      first_anim: `textAppears ${firstAnimDuration}s ${descriptionDelay}s forwards`,
+      second_anim: `textAppears ${firstAnimDuration}s ${secondAnimDelay}s forwards`,
+      third_anim: `textAppears ${thirdAnimDuration}s ${thirdAnimDelay}s forwards`,
+    }
   },
   plugins: [],
 }
